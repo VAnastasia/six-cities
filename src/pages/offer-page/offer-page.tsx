fdc0eda2db3110bf4308a2a7856e21dc180104d8
Anastasiya <v.anastasiya26@mail.ru>
2023-09-29 09:42:02 +0700

    fix disabled

--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -305,7 +305,7 @@ function OfferPage(): JSX.Element {
                     <button
                       className="reviews__submit form__submit button"
                       type="submit"
-                      disabled=""
+                      disabled
                     >
                   Submit
                     </button>
