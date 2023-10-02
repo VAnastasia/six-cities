51867b23d3e184b7137e50537559ec22b00366cd
Anastasiya <v.anastasiya26@mail.ru>
2023-10-02 20:52:38 +0700

    login page

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,6 +1,30 @@
+import { useRef, FormEvent } from 'react';
+import {useNavigate} from 'react-router-dom';
 import Logo from '../../components/logo/logo';
+import { useAppDispatch } from '../../hooks';
+import { loginAction } from '../../store/api-actions';
+import { AppRoute } from '../../const';
+// import {AppRoute} from '../../const';
 
 function LoginPage(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      dispatch(loginAction({
+        login: loginRef.current.value,
+        password: passwordRef.current.value
+      }));
+      navigate(AppRoute.Main);
+    }
+  };
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -16,10 +40,16 @@ function LoginPage(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form
+              className="login__form form"
+              action="#"
+              method="post"
+              onSubmit={handleSubmit}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input
+                  ref={loginRef}
                   className="login__input form__input"
                   type="email"
                   name="email"
@@ -30,6 +60,7 @@ function LoginPage(): JSX.Element {
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
                 <input
+                  ref={passwordRef}
                   className="login__input form__input"
                   type="password"
                   name="password"
@@ -38,7 +69,7 @@ function LoginPage(): JSX.Element {
                 />
               </div>
               <button className="login__submit form__submit button" type="submit">
-            Sign in
+                Sign in
               </button>
             </form>
           </section>
