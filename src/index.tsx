c623607e98c8dabe38b8a47e966664339f97191a
Anastasiya <v.anastasiya26@mail.ru>
2023-10-05 15:31:31 +0700

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HistoryRouter history={browserHistory} basename='/six-cities'>
+      <HistoryRouter history={browserHistory} basename='/six-cities/'>
         <ToastContainer />
         <App />
       </HistoryRouter>
